4508953fd5e805285d11e129213438ddcbb6f773
Denis <dobryj_denis00@mail.ru>
2024-09-27 15:15:53 +0300

    Working with pricing hero section

--- src/js/pricing.js
+++ src/js/pricing.js
@@ -1,13 +1,8 @@
-import '../assets/scss/main.scss';
+import '../assets/scss/pricing.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
-import { insightSlider, testimonialsSlider } from './components/home/slider.js';
 import { footerAccordion } from './components/home/accordion.js';
 
 useTheme();
 useBurger();
-insightSlider();
-testimonialsSlider();
 footerAccordion();
-
-
