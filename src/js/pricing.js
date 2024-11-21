70f03b609d0a2420873dbab3640ce3e5ace4e891
Denis <dobryj_denis00@mail.ru>
2024-11-21 21:45:27 +0300

    Finished pricing hero section

--- src/js/pricing.js
+++ src/js/pricing.js
@@ -2,7 +2,9 @@ import '../assets/scss/pricing.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { footerAccordion } from './components/home/accordion.js';
+import { usePriceSwitcher } from './components/pricing/pricing-switcher.js';
 
 useTheme();
 useBurger();
 footerAccordion();
+usePriceSwitcher();
