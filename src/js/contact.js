e96dd011dda76ee42e2dd41092bcc61d99fc2147
Denis <dobryj_denis00@mail.ru>
2024-11-22 15:10:46 +0300

    Create contact page and add hero contact section

---
+++ src/js/contact.js
@@ -0,0 +1,8 @@
+import '../assets/scss/contact.scss';
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { footerAccordion } from './components/home/accordion.js';
+
+useTheme();
+useBurger();
+footerAccordion();
