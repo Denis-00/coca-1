bad4c1839275872b066763c1b9aa6ad8ebb7d8bc
Denis <dobryj_denis00@mail.ru>
2024-11-22 19:40:27 +0300

    Add get-started section

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,8 +1,10 @@
 import '../assets/scss/contact.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { usePhone } from './components/contact/phone.js';
 import { footerAccordion } from './components/home/accordion.js';
 
 useTheme();
 useBurger();
+usePhone();
 footerAccordion();
